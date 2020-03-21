0af0f1bc6a4d65dc4a3649d853e804ac9af316ea
gomez714 <luey714@gmail.com>
2020-03-21 11:28:40 -0700

    built out layout grid, Header, and Navbar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Header from './header-navbar/header';
+import Navbar from './header-navbar/navbar';
+
+
 export default class Layout extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='layout'>
         {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }
