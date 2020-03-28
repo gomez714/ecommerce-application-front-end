277a73a3615076e12c31ebc2b4b777a29322b6b8
gomez714 <luey714@gmail.com>
2020-03-28 15:17:10 -0700

    built front of card and implemented rotation/flip animation ability

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -109,6 +109,20 @@ export function fetchShopProducts() {
                 description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal',
                 price: 1.99,
                 belongsTo: [0, 6]
+            },
+            {
+                id: 7,
+                title: 'Linux - How to Sort by Most Recently Updated',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal',
+                price: 1.99,
+                belongsTo: [0, 3]
+            },
+            {
+                id: 8,
+                title: 'Difference Between Models and Objects',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal',
+                price: 1.99,
+                belongsTo: [0, 3, 5]
             }
         ]
     })
--- src/components/shop/shop-product.js
+++ src/components/shop/shop-product.js
@@ -8,18 +8,29 @@ class ShopProduct extends Component {
         const { id, title, description, price } = this.props;
         return(
             <div key={id} className='shop-product'>
-                <div className='shop-product-title'>
-                    {title}
+
+                <div className="shop-product-front">
+                    <img className='shop-product-front-image' src="http://via.placeholder.com/220x220" />
+                    <div className="shop-product-front-title">
+                        {title}
+                    </div>
+
                 </div>
-                <div className='shop-product-description'>
-                    {description}
-                </div>
-                <GreenPriceTag className='shop-product-price' title={price} />
-                    
-                <Quantity className='shop-product-quantity' quantity={1}/>
-                <div className='shop-product-add-to-cart'>
-                    Add to Cart
-                </div>
+
+               <div className='shop-product-back'>
+                    <div className='shop-product-back-title'>
+                        {title}
+                    </div>
+                    <div className='shop-product-back-description'>
+                        {description}
+                    </div>
+                    <GreenPriceTag className='shop-product-back-price' title={price} />
+                        
+                    <Quantity className='shop-product-back-quantity' quantity={1}/>
+                    <div className='shop-product-back-add-to-cart'>
+                        Add to Cart
+                    </div>
+               </div>
             </div>
         )
     }
