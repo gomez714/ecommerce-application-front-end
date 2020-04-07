fd11fca169777197cb1695277d00177c52ec7564
gomez714 <luey714@gmail.com>
2020-04-06 19:22:45 -0700

    built review form buttons

---
+++ src/components/order/review-form.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+import {  FormButton } from '../form-fields';
+
+import history from '../../history';
+
+class ReviewForm extends Component {
+    render(){
+
+        const { className, handleSubmit } = this.props;
+
+    
+
+        return (
+            <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                <div className="review-form-line"></div>
+                
+                <Field className='review-form-proceed' 
+                onClick={() => history.push('/account')}
+                title="Proceed to checkout"
+                type="submit"
+                name="Proceed" 
+                component={FormButton}/>
+
+                <Field className='review-form-back' 
+                onClick={() => history.push('/sign-in')}
+                title="Back"
+                type="button"
+                short={true}
+                name="back" 
+                component={FormButton}/>
+
+            </form>
+        )
+    }
+}
+
+ReviewForm = reduxForm({
+    form: 'ReviewForm'
+})(ReviewForm);
+
+export default ReviewForm;
--- src/components/order/review.js
+++ src/components/order/review.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 import PageTitle from '../page-title';
+import ReviewForm from './review-form';
 
 class Review extends Component {
 
@@ -12,10 +13,15 @@ class Review extends Component {
         this.props.setNavbarLinks([]);
     }
 
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return(
             <div className='review'>
                 <PageTitle className='review-page-title' title='Order Review' />
+                <ReviewForm className='review-form' onSubmit={this.onSubmit} />
             </div>
         )
     }
