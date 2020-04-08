f84195934598fb5bd619998e112d1ec558a4aa3b
gomez714 <luey714@gmail.com>
2020-04-07 23:35:54 -0700

    layed out shipping form fields

--- src/components/information/payment.js
+++ src/components/information/payment.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
-import PaymentForm from './shipping-form';
+import PaymentForm from './payment-form';
 import PageTitle from '../page-title';
 
 
--- src/components/information/shipping.js
+++ src/components/information/shipping.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
-import ShippingForm from './payment-form';
+import ShippingForm from './shipping-form';
 import PageTitle from '../page-title';
 
 
